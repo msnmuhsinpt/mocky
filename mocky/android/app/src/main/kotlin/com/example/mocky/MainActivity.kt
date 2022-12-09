95c317c73e199051316df3845a427198c2e36edf
muhsin <msnmuhsin83@gmail.com>
2022-12-09 10:44:00 +0530

    initial

---
+++ mocky/android/app/src/main/kotlin/com/example/mocky/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mocky
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mocky/lib/util/app_color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColor {
+  static const kRed = Color(0xfff52829);
+  static const kGrey = Color(0xfff6f6f7);
+  static const kBalck = Color(0xff121331);
+  static const kLightGreen = Color(0xff3cce62);
+  static const kWhite = Color(0xffffffff);
+  static const kAddIconColor = Color.fromRGBO(121, 155, 79, 1);
+  static const kMintGreen = Color(0xff1DEFBD);
+  static const kGreen = Color.fromARGB(255, 3, 245, 15);
+  static const kLightBlack = Color.fromARGB(255, 15, 14, 14);
+}
